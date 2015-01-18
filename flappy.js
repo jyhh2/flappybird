dcaf6b3bfb4bc83950880ced0e8a1d3227b0508c
surface <huij93@gmail.com>
2015-01-19 03:46:34 +1100

    16:46

--- flappy.js
+++ flappy.js
@@ -7,17 +7,26 @@ var stateActions = { preload: preload, create: create, update: update };
 // - renderer (go for Phaser.AUTO)
 // - element where the game will be drawn ('game')
 // - actions on the game state (or null for nothing)
-var game = new Phaser.Game(900, 400, Phaser.AUTO, 'game', stateActions);
+var game = new Phaser.Game(800, 400, Phaser.AUTO, 'game', stateActions);
 
-var score = 0;
+var score = -1;
 var player;
+var pipes;
+
+var label_score;
 
 /*
  * Loads all resources for the game and gives them names.
  */
 function preload() {
-game.load.image("playerImg", "assets/watto3.png");
-game.load.audio("score","assets/out.mp3")
+    game.load.image("background", "assets/hawkeye2.jpg.png");
+    game.load.image("playerImg", "assets/watto3.png");
+game.load.audio("score","assets/out.mp3");
+
+    game.load.image("pipe", "assets/stumps50.png")
+    game.load.image("bottom","assets/bottom.png")
+    game.load.image("top","assets/top.png")
+
 }
 
 /*
@@ -26,42 +35,58 @@ game.load.audio("score","assets/out.mp3")
 function create() {
     // set the background colour of the scene
     game.stage.setBackgroundColor("#FFD6AD");
-    game.add.text(300,350, "Welcome to my game",
-        {font: "20px Arial", fill: "#FFFFFF"});
+
+    game.add.text(150,350, "LEG BEFORE WATTO",
+        {font: "20px Aharoni", fill: "#009900"});
+    game.add.text(100,370, "How many runs can he score?",
+        {font: "20px Aharoni", fill: "#009900"});
+    game.add.text(20,27.5, "SCORE",
+        {font: "20px Aharoni", fill: "#000000"});
 
 
 
-    game.input.onDown.add(clickHandler);
+    game.input.keyboard.addKey(Phaser.Keyboard.SPACEBAR).onDown.add(player_jump);
 
-    game.input.keyboard.addKey(Phaser.Keyboard.SPACEBAR).onDown.add(spaceHandler);
+    label_score = game.add.text(100,10, "0",
+    {font: "50px Aharoni", fill: "#000000"});
 
-    label_score = game.add.text(20,20, "0")
-
-    player = game.add.sprite(41,41, "playerImg");
+    player = game.add.sprite (450,200,"playerImg");
 
     game.input.keyboard.addKey(Phaser.Keyboard.RIGHT).onDown.add(moveRight);
     game.input.keyboard.addKey(Phaser.Keyboard.LEFT).onDown.add(moveLeft);
     game.input.keyboard.addKey(Phaser.Keyboard.DOWN).onDown.add(moveDown);
     game.input.keyboard.addKey(Phaser.Keyboard.UP).onDown.add(moveUp);
+
+    pipes = game.add.group();
+
+
+    generate_pipe();
+        pipe_interval = 1.75;
+        game.time.events.loop(pipe_interval * Phaser.Timer.SECOND, generate_pipe);
+
+    game.physics.startSystem(Phaser.Physics.ARCADE);
+
+    game.physics.arcade.enable(player);
+        player.body.velocity.x=0;
+        player.body.velocity.y=-100;
+        player.body.gravity.y=800
+
+
+
 }
+
 
 /*
  * This function updates the scene. It is called for every new frame.
  */
 function update() {
-    var label_score;
+    game.physics.arcade.overlap(player, pipes, game_over);
+
 }
 
-function clickHandler(event){
-    game.add.sprite(event.x, event.y, "playerImg");
-    game.sound.play("score");
-}
-function spaceHandler () {
-    game.sound.play("score");
-    changeScore();
-}
+
 function changeScore() {
-    score = score + 1;
+    score = score + (1/32);
     label_score.setText(score.toString());
 }
 function moveRight() {
@@ -77,3 +102,47 @@ function moveDown() {
 function moveUp() {
     player.y = player.y - 10;
 }
+
+function generate_pipe () {
+    var gap_start = game.rnd.integerInRange (0,3);
+    for (var count = -10; count <22; count++) {
+        if (count != gap_start && count != gap_start + 1 && count !=gap_start + 2 && count != gap_start + 3 && count != gap_start + 4) {
+            add_pipe_block(800, count * 50);
+        }
+        if(count == gap_start){
+            add_pipe_block2 (800, count *50)
+        }
+        if(count == gap_start + 4){
+            add_pipe_block3 (800, count *50)
+        }
+        changeScore();
+    }
+}
+
+function add_pipe_block (x,y) {
+    var pipe = pipes.create(x,y, "pipe");
+    game.physics.arcade.enable(pipe);
+    pipe.body.velocity.x=-200;
+}
+
+function add_pipe_block2 (x,y) {
+    var pipe = pipes.create(x,y, "bottom");
+    game.physics.arcade.enable(pipe);
+    pipe.body.velocity.x=-200;
+}
+
+function add_pipe_block3 (x,y) {
+    var pipe = pipes.create(x,y, "top");
+    game.physics.arcade.enable(pipe);
+    pipe.body.velocity.x=-200;
+}
+
+function player_jump() {
+    player.body.velocity.y=-300;
+}
+
+function game_over () {
+    game.sound.play("score")
+    location.reload();
+}
+
